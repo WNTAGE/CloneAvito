6b6d197aaa2790bd0e44b8cb19794ea0199062b1
Timurr <tik6051@gmail.com>
2024-01-24 21:18:06 +0600

    [+] Latvian lang

--- src/Utils/helpers/i18/i18n.ts
+++ src/Utils/helpers/i18/i18n.ts
@@ -1,22 +1,26 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import EnglishLanguage from '../../constants/InterfaceTranslate/English.json'
-import RussianLanguage from '../../constants/InterfaceTranslate/Russian.json'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import EnglishLanguage from "../../constants/InterfaceTranslate/English.json";
+import RussianLanguage from "../../constants/InterfaceTranslate/Russian.json";
+import LatvianLanguage from "../../constants/InterfaceTranslate/Latvian.json";
 
 i18n.use(initReactI18next).init({
-	resources: {
-		en: {
-			translation: EnglishLanguage,
-		},
-		ru: {
-			translation: RussianLanguage,
-		},
-	},
-	lng: 'en',
-	fallbackLng: 'en',
-	interpolation: {
-		escapeValue: false,
-	},
-})
+  resources: {
+    en: {
+      translation: EnglishLanguage,
+    },
+    ru: {
+      translation: RussianLanguage,
+    },
+    lv: {
+      translation: LatvianLanguage,
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-export default i18n
+export default i18n;
